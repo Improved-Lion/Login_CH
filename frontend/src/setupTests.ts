388a1dbd55781d8166c78fe284f8059404d74760
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2024-07-29 23:11:16 +0900

    test 파일 추가(실험중)

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -8,9 +8,6 @@ import "@testing-library/jest-dom";
     authorize: jest.fn(),
   },
 };
-// Set environment variables
-process.env.VITE_KAKAO_APP_KEY = "7c4b984298711ff20ce2635d66cf9367";
-process.env.VITE_REDIRECT_URL = "http://localhost:5173/login";
 
 // Mock import.meta.env for Vite
 (global as any).import = {
